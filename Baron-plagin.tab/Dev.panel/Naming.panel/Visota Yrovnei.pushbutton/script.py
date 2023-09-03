5c0fa9a38c69bd88d407fb38634261935872661c
Ab-dima <abankindima6116@gmail.com>
2023-09-03 18:09:33 +0300

    Update script.py

--- Baron-plagin.tab/Dev.panel/Naming.panel/Visota Yrovnei.pushbutton/script.py
+++ Baron-plagin.tab/Dev.panel/Naming.panel/Visota Yrovnei.pushbutton/script.py
@@ -2,7 +2,7 @@
 
 __title__ = 'Высота уровня'
 __author__ = 'Abankin Dmitry'
-__doc__ = 'Добавление высоты на уровень'
+__doc__ = 'Dobavlenie visoti'
 __highlight__ = 'new'
 __min_revit_ver__ = 2019
 __max_revit_ver__ = 2023
