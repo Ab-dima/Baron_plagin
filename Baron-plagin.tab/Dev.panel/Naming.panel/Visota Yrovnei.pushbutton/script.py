55e5e48994cda1bad803fdc518ab4b431a57906b
Ab-dima <abankindima6116@gmail.com>
2023-09-03 17:48:40 +0300

    Create script.py

---
+++ Baron-plagin.tab/Dev.panel/Naming.panel/Visota Yrovnei.pushbutton/script.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'Высота уровня'
+__author__ = 'Abankin Dmitry'
+__doc__ = 'Добавление высоты на уровень'
+__highlight__ = 'new'
+__min_revit_ver__ = 2019
+__max_revit_ver__ = 2023
+
+
+#IMPORTS
+
+import os,sys,math,datetime,time
+from Autodesk import *
+
+# PyRevit
+from pyrevit import revit, forms
+
+# Customs Imports
+
+
